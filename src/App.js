2c41fb58d3a3e08b07a7ab050590a128a195d240
Rushikesh Antala <Kishanpatal718@gmail.com>
2024-12-03 01:58:08 +0530

    Changes_old

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import Login from './LoginPage/Login'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Sidebar from './Components/Sidebar'
 
 function App() {
   return (
-    <div className="App">
-      <Login/>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/sidebar" element={<Sidebar />}>
+
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
